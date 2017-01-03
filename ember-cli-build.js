e10f9e36c39e9d4bb25b3e4024e815aca4d7386d
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-03 14:55:05 +0100

    ember-cli-build: Remove "StripTestSelectorsTransform" from registry
    This will be added automatically be the addon itself

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,22 +1,12 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-var StripTestSelectorsTransform = require('./strip-test-selectors');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     'ember-test-selectors': {
       environments: ['test']
     }
-  });
-
-  // add the StripTestSelectorsTransform to the registry, so the dummy app has
-  // it added in the HTMLBars build pipeline: all data-test-* attributes are
-  // therefore stripped
-  app.registry.add('htmlbars-ast-plugin', {
-    name: 'strip-test-selectors',
-    plugin: StripTestSelectorsTransform,
-    baseDir: function() { return __dirname; }
   });
 
   return app.toTree();
