a57d1e94982f2e78f30fba08ebda428e2d99aaae
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-12 11:38:28 +0100

    test-support: Increase "until" option in import deprecation

--- test-support/helpers/ember-test-selectors.js
+++ test-support/helpers/ember-test-selectors.js
@@ -6,7 +6,7 @@ let message = 'Importing testSelector() from "<appname>/tests/helpers/ember-test
 
 Ember.deprecate(message, false, {
   id: 'ember-test-selectors.test-selector-import',
-  until: '0.1.0',
+  until: '0.2.0',
   url: 'https://github.com/simplabs/ember-test-selectors#test-helpers',
 });
 
