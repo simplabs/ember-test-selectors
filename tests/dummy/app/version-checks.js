44e296472ba4c30412f80bd554e45fd2289b1d08
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-11-09 16:03:12 +0100

    Extract Ember version checks into separate module

---
+++ tests/dummy/app/version-checks.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const { VERSION } = Ember;
+
+const EMBERS_WITHOUT_RELIABLE_POSITIONAL_PARAMS = [
+  '2.2',
+  '2.1',
+  '2.0',
+  '1.13',
+  '1.12',
+  '1.11',
+];
+
+export const hasReliablePositionalParams = !EMBERS_WITHOUT_RELIABLE_POSITIONAL_PARAMS
+  .some(version => VERSION.indexOf(`${version}.`) === 0);
